bb6461ce7c311fad21637c4744a18781fc10e5f8
Shlomo <shlomoshnur@gmail.com>
2024-09-18 12:14:30 +0300

    Changed the paddle class to not inherit from Turtle, and to create two paddles in the init

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 from turtle import Screen
-
-from paddle import Paddle
+from paddle import Paddle, up, down
 from ball import Ball
 from scoreboard import Scoreboard
 import time
@@ -16,16 +15,15 @@ game_screen.title("Ping Pong Py")
 game_screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 game_screen.tracer(0)
 
-right_paddle = Paddle((350, 0))
-left_paddle = Paddle((-350, 0))
+paddles = Paddle()
 ball = Ball()
 score = Scoreboard()
 
 game_screen.listen()
-game_screen.onkey(right_paddle.up, "Up")
-game_screen.onkey(right_paddle.down, "Down")
-game_screen.onkey(left_paddle.up, "w")
-game_screen.onkey(left_paddle.down, "s")
+game_screen.onkey(lambda: up(paddles.right_paddle), "Up")
+game_screen.onkey(lambda: down(paddles.right_paddle), "Down")
+game_screen.onkey(lambda: up(paddles.left_paddle), "w")
+game_screen.onkey(lambda: down(paddles.left_paddle), "s")
 
 game_is_on = True
 while game_is_on:
@@ -38,18 +36,18 @@ while game_is_on:
         ball.bounce_floors()
 
     # collision with paddles
-    if (ball.distance(right_paddle) < 50 and ball.xcor() > SCREEN_WIDTH_BORDER - 60) or \
-            (ball.distance(left_paddle) < 50 and ball.xcor() < -SCREEN_WIDTH_BORDER + 60):
+    if (ball.distance(paddles.right_paddle) < 50 and ball.xcor() > SCREEN_WIDTH_BORDER - 60) or \
+            (ball.distance(paddles.left_paddle) < 50 and ball.xcor() < -SCREEN_WIDTH_BORDER + 60):
         ball.bounce_paddle()
 
     if ball.xcor() > SCREEN_WIDTH_BORDER:
         ball.create_ball()
         ball.bounce_paddle()
-        score.increase_score(left_paddle)
+        score.increase_score(paddles.left_paddle)
 
     if ball.xcor() < -SCREEN_WIDTH_BORDER:
         ball.create_ball()
         ball.bounce_paddle()
-        score.increase_score(right_paddle)
+        score.increase_score(paddles.right_paddle)
 
 game_screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,29 +1,38 @@
 from turtle import Turtle
 
 MOVEMENT = 20
-START_POSITION_X = 350
+START_POSITION_X_RIGHT = 350
+START_POSITION_X_LEFT = -350
 START_POSITION_Y = 0
 
 
-class Paddle(Turtle):
+def up(paddle):
+    new_ycor = paddle.ycor() + MOVEMENT
+    paddle.goto(paddle.xcor(), new_ycor)
 
-    def __init__(self, position=0):
-        super().__init__()
-        self.create_paddle(position)
 
-    def create_paddle(self, position=0):
-        if not position:
-            position = (START_POSITION_X, START_POSITION_Y)
-        self.color("white")
-        self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.penup()
-        self.goto(position)
+def down(paddle):
+    new_ycor = paddle.ycor() - MOVEMENT
+    paddle.goto(paddle.xcor(), new_ycor)
 
-    def up(self):
-        new_ycor = self.ycor() + MOVEMENT
-        self.goto(self.xcor(), new_ycor)
 
-    def down(self):
-        new_ycor = self.ycor() - MOVEMENT
-        self.goto(self.xcor(), new_ycor)
+class Paddle:
+
+    def __init__(self):
+        self.left_paddle = Turtle()
+        self.right_paddle = Turtle()
+        self.create_paddles()
+
+    def create_paddles(self):
+        position_right = (START_POSITION_X_RIGHT, START_POSITION_Y)
+        position_left = (START_POSITION_X_LEFT, START_POSITION_Y)
+        self.right_paddle.color("white")
+        self.left_paddle.color("white")
+        self.right_paddle.shape("square")
+        self.left_paddle.shape("square")
+        self.right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.right_paddle.penup()
+        self.left_paddle.penup()
+        self.right_paddle.goto(position_right)
+        self.left_paddle.goto(position_left)
